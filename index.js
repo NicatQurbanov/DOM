1d01d0b74e870715d85215d08e1b0d10367e6df3
Nicat <nicatqur1307@gmail.com>
2025-11-25 11:48:34 +0400

    to be continued

--- index.js
+++ index.js
@@ -1,74 +1,66 @@
-let users = {
-    user_1: {
-        url: ,
+let users = [
+    {
+        url: 'Images\Gandhi.webp',
         name: "Mahatma",
         surname: "Gandhi",
-        age: ,
-        job: ,
-        isMarried: ,
+        activity: "independence activist",
+        "birth-year": 1869,
     },
-    user_2: {
+    {
         url: 'Images\Malevich.jpeg',
         name: "Kazimir",
         surname: "Malevich",
-        age: ,
-        job: 'painter',
-        isMarried: ,
+        activity: 'painter',
+        "birth-year": 1879,
     },
-    user_3: {
+    {
         url: '.\Images\Henry-Ford.webp',
         name: "Henry",
         surname: "Ford",
-        age: ,
-        job: ,
-        isMarried: ,
+        activity: 'automobile manufacturer',
+        "birth-year": 1863,
     },
-    user_4: {
+    {
         url: 'Images\Steve.jfif',
         name: "Steve",
         surname: "Wozniak",
-        age: ,
-        job: 'programmer',
-        isMarried: ,
+        activity: 'programmer',
+        "birth-year": 1950,
     },
-    user_5: {
+    {
         url: 'Images\Ive.avif',
         name: "Jonahtan",
         surname: "Ive",
-        age: ,
-        job: 'designer',
-        isMarried: ,
+        activity: 'designer',
+        "birth-year": 1967,
     },
-    user_6: {
+    {
         url: 'Images\bill.jfif',
         name: "Bill",
         surname: "Gates",
-        age: ,
-        job: 'programmer',
-        isMarried: ,
+        activity: 'businessman',
+        "birth-year": 1955,
     },
-    user_7: {
+    {
         url: 'Images\jack.avif',
         name: "Jack",
         surname: "London",
-        age: ,
-        job: 'writer',
-        isMarried: ,
+        activity: 'novelist',
+        "birth-year": 1876,
     },
-    user_8: {
+    {
         url: 'Images\coco.jfif',
         name: "Coco",
         surname: "Chanel",
-        age: ,
-        job: 'designer',
-        isMarried: ,
+        activity: 'fashion designer',
+        "birth-year": 1883,
     },
-    user_9: {
+    {
         url: 'Images\picasso.jfif',
         name: "Pablo",
         surname: "Picasso",
-        age: ,
-        job: 'painter',
-        isMarried: ,
+        activity: ['painter', 'sculptor'],
+        "birth-year": 1881,
     }
-}
+]
+
